200c4bf9abda59e006e006002505895c82a63b2d
RomanDG <romand85@yandex.ru>
2019-06-27 09:28:43 +0300

    добавил значения по умолчанию

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Test.create(title: "Default Title")
+Answer.create(body: "Default Answer", correct: false)
