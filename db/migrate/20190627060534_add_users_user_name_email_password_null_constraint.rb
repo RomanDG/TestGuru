93b494b48db66b3883b61baf355365932aab9d5e
RomanDG <romand85@yandex.ru>
2019-07-02 19:46:57 +0300

    исправил все замечания к заданию

--- db/migrate/20190627060534_add_users_user_name_email_password_null_constraint.rb
+++ db/migrate/20190627060534_add_users_user_name_email_password_null_constraint.rb
@@ -1,13 +1,7 @@
 class AddUsersUserNameEmailPasswordNullConstraint < ActiveRecord::Migration[5.2]
-  def up
+  def change
   	change_column_null :users, :user_name, false
   	change_column_null :users, :email, false
   	change_column_null :users, :password, false
-  end
-
-  def down
-  	change_column_null :users, :user_name, true
-  	change_column_null :users, :email, true
-  	change_column_null :users, :password, true
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Test.create(title: "Default Title")
-Answer.create(body: "Default Answer", correct: false)
+# Test.create(title: "Default Title")
+# Answer.create(body: "Default Answer", correct: false)
