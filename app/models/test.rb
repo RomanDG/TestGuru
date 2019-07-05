c7ae719038763bb8acd5ff35cb06fd7f75ae4e3f
RomanDG <romand85@yandex.ru>
2019-07-05 14:16:54 +0300

    решил проблему с табами

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def ordered_by_category category
-		Test.order(title: :desc).joins("join categories on tests.category_id = categories.id").where(categories: { title: category }).pluck(:title)
-	end
+    Test.order(title: :desc).joins("join categories on tests.category_id = categories.id").where(categories: { title: category }).pluck(:title)
+  end
 end
--- db/migrate/20190704171824_change_tests_level_column_type.rb
+++ db/migrate/20190704171824_change_tests_level_column_type.rb
@@ -1,5 +1,5 @@
 class ChangeTestsLevelColumnType < ActiveRecord::Migration[5.2]
   def change
-  	change_column :tests, :level, :integer
+    change_column :tests, :level, :integer
   end
 end
