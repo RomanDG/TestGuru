838549857bfbbc6853a3835161751923f96437c3
RomanDG <romand85@yandex.ru>
2019-07-09 12:35:00 +0300

    добавил инстанс метод в модель User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-
+  def user_tests_list level
+    Test.select(:title).joins(:users).where(users: {id: self.id}).where(tests: {level: level})
+  end
 end
