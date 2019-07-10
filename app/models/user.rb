1d9e7a25efc35cee7ae4daec6fb9bf67ce7c4010
RomanDG <romand85@yandex.ru>
2019-07-10 15:54:24 +0300

    исправил, недочет (относительно ассоциации теста и автора)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
+  has_many :tests
 
-  def user_tests_list level
+  def tests_by_level level
     Test.select(:title).joins(:users).where(users: {id: self.id}).where(tests: {level: level})
   end
 end
