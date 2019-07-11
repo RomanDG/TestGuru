b072a42c7d6e3c95f6182eb31b98b10f70fa7f21
RomanDG <romand85@yandex.ru>
2019-07-11 14:34:38 +0300

    исправил недочет с ассоциациями

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :tests
 
   def tests_by_level level
     Test.select(:title).joins(:users).where(users: {id: self.id}).where(tests: {level: level})
