912f7adf4aba579140de4c27483de1333e943963
RomanDG <romand85@yandex.ru>
2019-08-25 20:43:25 +0300

    задание 6, пункт 1

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,10 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def self.ordered_by_category category
     Test.order(title: :desc).joins("join categories on tests.category_id = categories.id").where(categories: { title: category }).pluck(:title)
   end
