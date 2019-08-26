524c134d29e6f911a732e14fc76fcbe11beb64f4
RomanDG <romand85@yandex.ru>
2019-08-26 09:24:18 +0300

    задание 6, пункт 3

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,10 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :level, message: "should be one test with a given name and level"}
+  validates_numericality_of :level, greater_than_or_equal_to: 0
+
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
+  validates :user_name, :email, :password, presence: true
+
   def tests_by_level level
     # Test.select(:title).joins(:users).where(users: {id: self.id}).where(tests: {level: level})
     tests.select(:title).where(level: level)
