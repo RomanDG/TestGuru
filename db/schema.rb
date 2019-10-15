b975cc9f223de7f9b66f0a8a580fc323a44e8da9
RomanDG <romand85@yandex.ru>
2019-10-15 21:22:45 +0300

    очередные доработки

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_09_19_064634) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "level", default: 0
+    t.integer "level", default: 0, null: false
     t.integer "author_id", default: 0, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
